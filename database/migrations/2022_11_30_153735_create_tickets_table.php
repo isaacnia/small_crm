009c005674ab9f17cfb8bd1625997b3df4d8ea13
Rasool Isaacnia <r.isaacnia@gmail.com>
2022-11-30 20:47:48 +0330

    Add tickets table migration

---
+++ database/migrations/2022_11_30_153735_create_tickets_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->text('description');
+            $table->set('priority',['Critical','High','Medium','Low','Info']);
+            $table->set('status',['Open','Close','Reopen','Lock']);
+            $table->foreignId('assigned')
+                    ->constrained('users')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+            $table->set('categories',['']);
+            $table->set('labels',['']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
